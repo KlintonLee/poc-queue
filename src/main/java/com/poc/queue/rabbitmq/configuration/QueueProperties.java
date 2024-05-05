f5fb39b8283996fe9c25a8e4e60df75fbcad8f9c
klinton lee <klintonlee92@gmail.com>
2024-05-04 23:50:38 -0300

    fix: add configuration properties to QueueProperties

--- src/main/java/com/poc/queue/rabbitmq/configuration/QueueProperties.java
+++ src/main/java/com/poc/queue/rabbitmq/configuration/QueueProperties.java
@@ -3,7 +3,9 @@ package com.poc.queue.rabbitmq.configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix = "amqp.queues.image-created")
 public class QueueProperties implements InitializingBean {
 
     private static final Logger logger = LoggerFactory.getLogger(QueueProperties.class);
