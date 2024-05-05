fdaedf2a7fbdec5fc883382dc26fe0ef4eb8d84c
klinton lee <klintonlee92@gmail.com>
2024-05-05 04:09:08 -0300

    chore: add log for image media service

--- src/main/java/com/poc/queue/rabbitmq/service/ImageMediaService.java
+++ src/main/java/com/poc/queue/rabbitmq/service/ImageMediaService.java
@@ -1,19 +1,23 @@
 package com.poc.queue.rabbitmq.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.poc.queue.rabbitmq.amqp.ImageCreateEvent;
 import com.poc.queue.rabbitmq.dto.ImageMediaDto;
 import com.poc.queue.rabbitmq.models.ImageMedia;
 import com.poc.queue.rabbitmq.models.MediaStatus;
 import com.poc.queue.rabbitmq.repository.ImageMediaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 @Service
 public class ImageMediaService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageMediaService.class);
 
     private final ImageMediaRepository repository;
 
@@ -24,15 +28,16 @@ public class ImageMediaService {
         this.eventService = Objects.requireNonNull(eventService);
     }
 
-    public ImageMedia save(final ImageMediaDto imageMedia) throws JsonProcessingException {
+    public ImageMedia save(final ImageMediaDto imageMedia) throws IOException {
         final var id = UUID.randomUUID().toString();
         final var fileName = imageMedia.file().getOriginalFilename();
         final var contentType = imageMedia.file().getContentType();
         final var image = ImageMedia.create(id, fileName, contentType, null, MediaStatus.PENDING);
-        final var imageEvent = ImageCreateEvent.from(id, imageMedia.file());
+        final var imageEvent = ImageCreateEvent.from(id, imageMedia.file().getBytes());
 
         this.repository.save(image);
         eventService.send(imageEvent);
+        LOGGER.info("Image created: " + image.toString());
         return image;
     }
 
